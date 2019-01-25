807b9b201129f34306a147a5b63267a44b739110
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-25 13:48:36 -0700

    added email validation and finished redux form for sign up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LoginForm from './components/login-form';
+import SignupForm from './components/signup-form';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <LoginForm />
+        <SignupForm />
       </div>
     );
   }
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Field, reduxForm } from 'redux-form';
+import renderField from './renderField';
+import {required, nonEmpty, isTrimmed, length, matches, email} from '../validators';
+
+const usernameLength = length({min: 3, max: 40});
+const passwordLength = length({min: 8, max: 40});
+const passwordMatch = matches('password');
 
 export class SignupForm extends React.Component{
+  submit = (values) => {
+    values.preventDefault();
+  }
   render(){
     return(
-      <form>
-        <label htmlFor='username'>Username</label>
-        <Field name='username' component='input' type='text' />
-        <label htmlFor='email'>Email</label>
-        <Field name='email' component='input' type='email' />
-        <label htmlFor='password'>Password</label>
-        <Field name='password' component='input' type='text' />
-        <label htmlFor='confirmPassword'>Confirm Password</label>
-        <Field name='confirmPassword' component='input' type='text' />
+      <form onSubmit={this.props.handleSubmit(values => this.submit(values))}>
+        <Field name='username' component={renderField} type='text' label='Username' validate={[required, nonEmpty, isTrimmed, usernameLength]}/>
+        <Field name='email' component={renderField} type='text' label='Email' validate={[required, nonEmpty, email]}/>
+        <Field name='password' component={renderField} type='password' label='Password' validate={[required, nonEmpty, isTrimmed, passwordLength]}/>
+        <Field name='confirmPassword' component={renderField} type='password' label='Confirm Password' validate={[required, nonEmpty, passwordMatch]}/>
+        <button type='submit'>Signup</button>
       </form>
     )  
   }
