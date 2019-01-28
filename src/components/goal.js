f8fa3833e2486ea2ddfcd2e2e37f9615571572cd
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-28 13:43:18 -0700

    changed progress bar on goal to be connected to state instead of props

--- src/components/goal.js
+++ src/components/goal.js
@@ -19,8 +19,8 @@ export default class Goal extends React.Component{
       <div className='goal'>
         <h3>{this.props.title}</h3>
         <div className='graph'>
-          <div className='progress-bar' style={{width: `${this.props.complete*100/this.props.goal}%`}}>
-          {`${this.props.complete*100/this.props.goal}%`}
+          <div className='progress-bar' style={{width: `${this.state.complete*100/this.props.goal}%`}}>
+          {`${this.state.complete*100/this.props.goal}%`}
           </div>
         </div>
         <input type='number' value={this.state.complete} onChange={e => this.setComplete(e)} min={0} max={this.props.goal}></input><span>{` ${this.props.unit}`}</span>
