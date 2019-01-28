bcefb4208db80c0cf2526413bbf882539a5fe94b
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-28 13:01:39 -0700

    finished basic add goal form

--- src/components/addGoal-form.js
+++ src/components/addGoal-form.js
@@ -6,7 +6,9 @@ export default class AddGoalForm extends React.Component{
     this.state = {
       category: '',
       goal: '',
-      unit: ''
+      unit: '',
+      goalNumber: '',
+      goalType: ''
     }
   }
 
@@ -18,7 +20,35 @@ export default class AddGoalForm extends React.Component{
   setGoal(e){
     this.setState({
       goal: e.currentTarget.value
-    })
+    });
+  }
+
+  setUnit(e){
+    this.setState({
+      unit: e.currentTarget.value
+    });
+  }
+
+  setGoalNumber(e){
+    this.setState({
+      goalNumber: e.currentTarget.value
+    });
+  }
+
+  setGoalType(e){
+    this.setState({
+      goalType: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(`${{category: this.state.category,
+      goal: this.state.goal,
+      unit: this.state.unit,
+      goalNumber: this.state.goalNumber,
+      goalType: this.state.goalNumber
+    }}`)
   }
 
   render(){
@@ -27,10 +57,22 @@ export default class AddGoalForm extends React.Component{
       'Health': ['Weight Loss']
     };
     const goalDictionary = {
-      'Run': ['km', 'miles'],
-      'Pushups': ['reps'],
-      'Situpes': ['reps'],
-      'WeightLoss': ['kg', 'lb']
+      'Run': {
+        units: ['km', 'miles'],
+        type: ['Work Up To', 'Total']
+      },
+      'Pushups': {
+        units: ['Reps'],
+        type: ['Work Up To', 'Total']
+      },
+      'Situps': {
+        units: ['Reps'],
+        type: ['Work Up To', 'Total']
+      },
+      'Weight Loss': {
+        units: ['kg', 'lb'],
+        type: ['Total']
+      }
     };
     const categoryChoices = ['Fitness', 'Health'].map(cat => <option key={cat}>{cat}</option>);
     let goalPortion;
@@ -41,13 +83,36 @@ export default class AddGoalForm extends React.Component{
           {goalChoices}
         </select>;
     }
+    let readyToSubmit = false;
+    if(this.state.unit && this.state.goalType && this.state.goalNumber){
+      readyToSubmit = true;
+    }
+    let specifics;
+    if(this.state.goal){
+      const unitChoices = goalDictionary[this.state.goal].units.map(unit => <option key={unit}>{unit}</option>);
+      const typeChoices = goalDictionary[this.state.goal].type.map(type => <option key={type}>{type}</option>)
+      specifics = <div>
+        <input type='integer' onChange={(e) => this.setGoalNumber(e)}></input>
+        <select onChange={(e) => this.setUnit(e)}>
+        <option value=''>Choose units:</option>
+        {unitChoices}
+        </select>
+        <br/>
+        <select onChange={(e) => this.setGoalType(e)}>
+          <option value=''>Choose Goal Type:</option>
+          {typeChoices}
+        </select>
+        <button type='submit' disabled={!readyToSubmit}>Add Goal</button>
+      </div>
+    }
     return (
-      <form>
+      <form onSubmit={e => this.handleSubmit(e)}>
         <select onChange={(e) => this.setCategory(e)}>
           <option value=''>Choose a goal category:</option>
           {categoryChoices}
         </select>
         {goalPortion}
+        {specifics}
       </form>
     )
   }
