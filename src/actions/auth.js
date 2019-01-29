5a31f9ae8da21f4c938d670934d981462410c9ba
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-29 16:12:49 -0700

    added auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,43 @@
+import jwtDecode from 'jwt-decode';
+
+import {API_BASE_URL} from '../config';
+import {normalizeResponseErrors} from './utils';
+
+import {saveAuthToken, clearAuthToken} from '../local-storage';
+
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const setAuthToken = authToken => ({
+  type: SET_AUTH_TOKEN,
+  authToken
+});
+
+export const CLEAR_AUTH = 'CLEAR_AUTH';
+export const clearAuth = () => ({
+  type: CLEAR_AUTH
+});
+
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const authRequest = () => ({
+  type: AUTH_REQUEST
+});
+
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const authSuccess = currentUser => ({
+  type: AUTH_SUCCESS,
+  currentUser
+});
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const authError = error => ({
+  type: AUTH_ERROR,
+  error
+});
+
+// Stores the auth token in state and localStorage, and decodes and stores
+// the user data stored in the token
+const storeAuthInfo = (authToken, dispatch) => {
+  const decodedToken = jwtDecode(authToken);
+  dispatch(setAuthToken(authToken));
+  dispatch(authSuccess(decodedToken.user));
+  saveAuthToken(authToken);
+};
