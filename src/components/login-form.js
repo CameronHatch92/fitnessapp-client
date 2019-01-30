f3c70d1b3a0f01d7ca86213518ce7e1763e52053
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-30 14:17:17 -0700

    added login mutation to login form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {Field, reduxForm } from 'redux-form';
+import { Mutation } from 'react-apollo';
+import { Field, reduxForm } from 'redux-form';
 import renderField from './renderField';
-import {required, nonEmpty} from '../validators';
+import { required, nonEmpty } from '../validators';
+import { LOGIN_USER } from '../Mutations';
 
-export class LoginForm extends React.Component{
+export class LoginForm extends React.Component {
   submit = (values) => {
     values.preventDefault();
     console.log(values.currentTarget.username.value);
     console.log(values.currentTarget.password.value);
   }
-  render(){
-    return(
-      <form onSubmit={this.props.handleSubmit(values => this.submit(values))}>
-        <Field name='username' component={renderField} type='text' label='Username' validate={[required, nonEmpty]}/>
-        <Field name='password' component={renderField} type='text' label='Password' validate={[required, nonEmpty]}/>
-        <button type='submit'>Login</button>
-      </form>
-    )  
+  render() {
+    return (
+      <Mutation mutation={LOGIN_USER}>
+        {(login, {data}) => (
+          <form onSubmit={e=> {
+            e.preventDefault();
+            login({variables: {username: e.currentTarget.username.value, password: e.currentTarget.password.value}});
+            console.log(data);
+          }}>
+            <Field name='username' component={renderField} type='text' label='Username' validate={[required, nonEmpty]} />
+            <Field name='password' component={renderField} type='text' label='Password' validate={[required, nonEmpty]} />
+            <button type='submit'>Login</button>
+          </form>
+        )}
+      </Mutation>
+    )
   }
 }
 
